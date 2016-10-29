ee854cf31bdeda2ad95888f42032dc3dc2fa2211
logosha <logosha@gmail.com>
2016-10-29 13:16:28 +0300

    define my location in service

--- app/src/main/java/com/google/pizzahot/services/CallService.java
+++ app/src/main/java/com/google/pizzahot/services/CallService.java
@@ -1,7 +1,15 @@
 package com.google.pizzahot.services;
 
+import android.Manifest;
 import android.app.IntentService;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 
 import com.google.gson.Gson;
@@ -22,40 +30,61 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 
-public class CallService extends IntentService {
+public class CallService extends IntentService implements LocationListener {
 
     private static final String CLIENT_ID = "ZURIFJGOKBITYRPLBHZHRSXTHAFGYADA52RJRG5HHJPNVKXS";
     private static final String CLIENT_SECRET = "ZV21NT0DOHPKTKURHHOYNABHG14CLQ32J0WIQAADKZBQFNCH";
     private static final String VERSION = "20130815";
     private static final String LIMIT = "10";
     private static final String QUERY = "pizza";
-    private static final String latitude = "40.7463956";
-    private static final String longtitude = "-73.9852992";
+    private static final String latitudeNY = "40.7463956";
+    private static final String longtitudeNY = "-73.9852992";
     private static final String TAG = "myLogs";
 
-    private static final String URL = "https://api.foursquare.com/v2/venues/search?client_id="
-            + CLIENT_ID + "&client_secret=" + CLIENT_SECRET + "&v=" + VERSION + "&limit=" + LIMIT + "&ll=" + latitude + "," + longtitude + "&query=" + QUERY;
+
+
 
     private static final Gson jsonMarshaller = new GsonBuilder().create();
-
-
     ArrayList<FoursquareRestaurant> pizzaList;
+
+    private LocationManager locationManager;
+    private Location location;
+    private double latitude;
+    private double longtitude;
+
 
     public CallService() {
         super("CallService");
     }
 
 
+    public Location getLocation() {
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                  }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
+        return location;
+    }
+
+
+
     @Override
     protected void onHandleIntent(Intent intent) {
 
+        String url = "https://api.foursquare.com/v2/venues/search?client_id="
+                + CLIENT_ID + "&client_secret=" + CLIENT_SECRET + "&v=" + VERSION + "&limit=" + LIMIT + "&ll=" + latitude + "," + longtitude + "&query=" + QUERY;
+
+        latitude = location.getLatitude();
+        longtitude = location.getLongitude();
 
         OkHttpClient client = new OkHttpClient();
         String jsonBody = null;
         Response response;
         try {
             Request request = new Request.Builder()
-                .url(URL)
+                .url(url)
                 .build();
 
             response = client.newCall(request).execute();
@@ -91,10 +120,25 @@ public class CallService extends IntentService {
             // TODO убрать прогресс, сообщить об ошибке.
         }
 
-
-
         return null;
+    }
 
+
+    @Override
+    public void onLocationChanged(Location location) {
+        this.location = location;
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
     }
 
 
